6ea4a3bd9deca79eb1f41f2af285a09b99150663
yuan356 <59214624+yuan356@users.noreply.github.com>
2020-09-04 13:16:40 +0800

    Initial Commit

---
+++ VideoPlayerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayerApp
+//
+//  Created by 阿遠 on 2020/9/4.
+//  Copyright © 2020 yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
